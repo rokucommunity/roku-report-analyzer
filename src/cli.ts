72574b5d5b51168654a8e02f50dbbc785c918627
Bronley Plumb <bronley@gmail.com>
2022-03-01 08:14:32 -0500

    Fix cli to match options

--- src/cli.ts
+++ src/cli.ts
@@ -8,8 +8,7 @@ let options = yargs
     .option('$0 [crashlogs..]', { type: 'array', defaultDescription: 'true', description: 'One or more globs that match to crash logs. Any zip archives encountered will be extracted, and all files inside will be processed.' })
     .option('cwd', { type: 'string', description: 'Override the current working directory' })
     .option('outDir', { type: 'string', description: 'The directory where the processed logs should be written', defaultDescription: './dest' })
-    .option('project', { type: 'array', defaultDescription: 'true', description: 'Creates a zip package. This setting is ignored when deploy is enabled.' })
-    .option('libs', { type: 'array', defaultDescription: 'true', description: 'One or more component library folders that contain sourcemaps. Will scan for a manifest file to identify sg_component_libs_provided. You can override/supply sg_component_libs_provided by prefixing the path with the sg_component_libs_provided value and a semicolon. (i.e. "--libs YourComplib;../path/to/complib"' })
+    .option('projects', { type: 'array', defaultDescription: 'true', description: 'Paths to source code folders, optionally prefixed with sg_component_libs_provided. For example: ["C:/projects/mainApp", "./complib1", "complib2:C:/projects/complib2"]' })
     .argv;
 
 const runner = new Runner(options as any);
